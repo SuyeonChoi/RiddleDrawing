8c88b55df0349120573929c4c4f693277c2ca9cf
seong954t <seomtae@naver.com>
2019-07-09 12:23:56 +0900

    drawable_canvas clear 예제 추가

--- example/drawable_canvas/main.js
+++ example/drawable_canvas/main.js
@@ -1,3 +1,6 @@
 window.onload = function(){
     window.drawableCanvas.initialize('drawable_canvas');
+    document.querySelector('#remove-btn').addEventListener('click', function(){
+        window.drawableCanvas.clear();
+    })
 }
