476e7769c81c0b52605788baa3ab5090e768a747
Seoungtae Kim <seomtae@naver.com>
2019-07-11 18:43:53 +0900

    삭제된 파일 복구

---
+++ static/js/creatable_canvas.js
@@ -0,0 +1,32 @@
+/**
+ * creatable_canvas.js with sketch_rnn.js & p5.js & p5.svg.js
+ * https://github.com/seong954t/RiddleDrawing
+ *
+ * Copyright 2019, Seungtae Kim
+ * under the MIT license.
+ */
+(function() {
+    const z_0 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    function initialize(width, height){
+
+    }
+
+    function drawImage(imageData, color){
+
+    }
+
+    const creatableCanvas = {
+        initialize: initialize,
+        drawImage: drawImage
+    };
+
+    if (typeof define === 'function' && define.amd) {
+        define(function() {
+            return creatableCanvas;
+        });
+    } else if (typeof module !== 'undefined') {
+        module.exports = creatableCanvas;
+    } else {
+        window.creatableCanvas = creatableCanvas;
+    }
+})();
