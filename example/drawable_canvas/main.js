32ac8625c52253086528d2d3ff33bf1739974f44
HyoinJeong <hiamy0107@naver.com>
2019-07-11 17:56:56 +0900

    main.html 업로드

--- example/drawable_canvas/main.js
+++
@@ -1,6 +0,0 @@
-window.onload = function(){
-    window.drawableCanvas.initialize('drawable_canvas');
-    document.querySelector('#remove-btn').addEventListener('click', function(){
-        window.drawableCanvas.clear();
-    })
-}
