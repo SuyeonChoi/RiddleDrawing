9a1b46802408eb75b665f40366a67dbcf7693c88
suyeonchoi <tndus970120@gmail.com>
2019-07-12 09:51:13 +0900

    ranking skeleton CSS 추가

--- Ranking.js
+++ Ranking.js
@@ -1,17 +1,32 @@
 
 var btn = document.getElementById("openRank");
 btn.onclick = function popRankBox(){
-  //var modal_backgroud = document.createElemet("div");
-  //modal_backgroud.setAttribute("id","modal_backgroud");
-  //modal_backgroud.setAttribute("class","modal");
+  var modal_backgroud = document.body;
+  modal_backgroud.style.display = "block";
+  modal_backgroud.style.position = "fixed";
+  modal_backgroud.style.left = 0;
+  modal_backgroud.style.right = 0;
+  modal_backgroud.style.widht = "100%";
+  modal_backgroud.style.height = "100%";
+  modal_backgroud.style.backgroundColor = "rgba(0,0,0,0.4)";
 
   var modal_content = document.createElement("div");
   modal_content.setAttribute("class","modal_content");
+  modal_content.style.backgroundColor = "#fefefe";
+  modal_content.style.margin = "auto";
+  modal_content.style.padding = "40px";
+  modal_content.style.border = "1px solid #888";
+  modal_content.style.width = "70%";
+  modal_content.style.height = "50%";
 
   var close = document.createElement("span");
   close.setAttribute("class","close");
   close.innerHTML = "&times;";
+  close.style.color = "#aaaaaa";
+  close.style.float = "right";
+  close.style.fontSize = "28px";
+  close.style.fontWeight = "bold";
 
   modal_content.appendChild(close);
-  document.getElementById("popModal").appendChild(modal_content);
+  modal_backgroud.appendChild(modal_content);
 }
