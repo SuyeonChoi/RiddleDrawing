438470f9e75c4d3973c02feb7f05114c48014969
suyeonchoi <tndus970120@gmail.com>
2019-07-12 10:12:53 +0900

    close button css 수정 및 기능 추가

--- Ranking.js
+++ Ranking.js
@@ -8,7 +8,6 @@ btn.onclick = function popRankBox(){
   modal_backgroud.style.right = 0;
   modal_backgroud.style.widht = "100%";
   modal_backgroud.style.height = "100%";
-  modal_backgroud.style.backgroundColor = "rgba(0,0,0,0.4)";
 
   var modal_content = document.createElement("div");
   modal_content.setAttribute("class","modal_content");
@@ -18,21 +17,25 @@ btn.onclick = function popRankBox(){
   modal_content.style.border = "1px solid #888";
   modal_content.style.width = "70%";
   modal_content.style.height = "50%";
+  modal_content.style.backgroundColor = "rgba(0,0,0,0.2)";
 
   var close = document.createElement("span");
   close.setAttribute("class","close");
   close.innerHTML = "&times;";
-  close.style.color = "#aaaaaa";
+  close.style.color = "black";
   close.style.float = "right";
   close.style.fontSize = "28px";
   close.style.fontWeight = "bold";
-
   close.onmouseover = function mouseOver(){
-    close.style.color = "#000";
+    close.style.color = "rgba(0,0,0,0.5)";
     close.style.cursor = "pointer";
   }
   close.onmouseout = function mouseOut(){
-    close.style.color = "#aaaaaa";
+    close.style.color = "black";
+  }
+
+  close.onclick = function(){
+    modal_backgroud.removeChild(modal_content);
   }
 
   modal_content.appendChild(close);
