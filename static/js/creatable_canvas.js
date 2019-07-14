a134b8d85f2c1ab460eb4fb6bfd9d8151a597e1d
seong954t <seomtae@naver.com>
2019-07-14 15:11:32 +0900

    그림 생성 모듈 추가

--- static/js/creatable_canvas.js
+++ static/js/creatable_canvas.js
@@ -6,13 +6,33 @@
  * under the MIT license.
  */
 (function() {
-    const z_0 = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
     function initialize(width, height){
-
+        createCanvas(width, height);
     }
 
-    function drawImage(imageData, color){
+    function drawImage(imageData, color, weight){
+        let x = canvas.width/4;
+        let y = canvas.height/4;
+        let dx, dy;
+        let pen_down = 0, pen_up = 1, pen_end = 0;
 
+        clear();
+        for(let i=0; i<imageData.length; i++) {
+            [dx, dy, pen_down, pen_up, pen_end] = imageData[i];
+        
+            if (pen_end == 1) {
+                break;
+            }
+    
+            if (pen_down == 1) {
+                stroke(color);
+                strokeWeight(weight);
+                line(x, y, x+dx, y+dy);
+            }
+        
+            x += dx;
+            y += dy;
+        }
     }
 
     const creatableCanvas = {
